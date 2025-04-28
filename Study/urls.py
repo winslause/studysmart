9119af727627b94973da8e4c471122266d096628
wenslause <wenbusale383@gmail.com>
2025-04-28 17:43:18 +0300

    8

--- Study/urls.py
+++ Study/urls.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.i18n import set_language
+from django.contrib.sitemaps.views import sitemap
+from core.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # Assuming your app's URLs are in core/urls.py
-    path('i18n/', set_language, name='set_language'),  # Add set_language URL
+    path('', include('core.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('i18n/', include('django.conf.urls.i18n')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
